0f01b4c674ed950781d5a0bc415c8bc01b4aae96
boazblake <boazblake@gmail.com>
2016-07-01 09:47:05 -0500

    basic get and post routing is working

--- server.js
+++ server.js
@@ -25,10 +25,21 @@ app.set('view engine', 'ejs')
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded({extended: true}) );
 
+//MONGOOSE///
+//new mongoose instance
+const db = mongoose.connect('mongodb://localhost/productsDB')
+
+let Product = require('./src/scripts/models/product')
 
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
+
+router.use((req, res, next) =>  {
+  //do logging
+  console.log('something is happening');
+  next(); // make sure we go to the next routes and don't stop here
+})
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
@@ -37,48 +48,62 @@ router.get('/', function(req, res) {
 
 // more routes for our API will happen here
 
+// on routes that end in /products
+// ----------------------------------------------------
+router.route('/products')
+
+    // create a product (accessed at POST http://localhost:8080/api/products)
+    .post(function(req, res) {
+        var product = new Product();      // create a new instance of the Product model
+        product.name = req.body.name;  // set the products name (comes from the request)
+
+        // save the product and check for errors
+        product.save(function(err) {
+            if (err)
+                res.send(err);
+            res.json({ message: 'Product created!' });
+        });
+    })
+
+    // get all the products (accessed at GET http://localhost:8080/api/products)
+    .get(function(req, res) {
+        Product.find(function(err, products) {
+            if (err)
+                res.send(err);
+
+            res.json(products);
+        });
+    });
+
+
+
+
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/api', router);
-
-//MONGOOSE///
-//new mongoose instance
-const db = mongoose.connect('mongodb://localhost/productsDB')
-
-//mongoose model
-let productsSchema = new Schema({
-  imgLink:String,
-  size: Number,
-  name:String,
-  uploadDate: Number,
-  description:String,
-  fileName:String
-})
+app.use('/api/v1', router);
 
 
-let ProductsModel =  mongoose.model('Dummy', productsSchema)
+// //Fetch from DB
+// app.get('/api/v1/products', function(req, res) {
+//   ProductsModel.find({}, function(err, products){
+//     if(err) res.status(500).send(err)
+//       else res.json(products)
+//   })
+// })
 
-//Fetch from DB
-app.get('/api/v1/products', function(req, res) {
-  ProductsModel.find({}, function(err, products){
-    if(err) res.status(500).send(err)
-      else res.json(products)
-  })
-})
-
-//POST to DB//
-app.post('/api/v1/products', function(req, res) {
-  req.body.uploadedAt = new Date()
+// //POST to DB//
+// app.post('/api/v1/products', function(req, res) {
+//   req.body.uploadedAt = new Date()
   
-  let newRecord = new ProductsModel(req.body)
-  newRecord.save()
-  res.status(201).send(newRecord)
+//   let newRecord = new ProductsModel(req.body)
+//   newRecord.save()
+//   res.status(201).send(newRecord)
 
-  res.json(req.body)
-})
+//   res.json(req.body)
+// })
 
 
-//REMOVE from DB
+// //REMOVE from DB
 
 
 
--- src/scripts/models/product.js
+++ src/scripts/models/product.js
@@ -1,3 +1,17 @@
+'use strict'
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+//mongoose model
+let ProductSchema = new Schema({
+  imgLink:String,
+  size: Number,
+  name:String,
+  uploadDate: Number,
+  description:String,
+  fileName:String
+})
+
+
+module.exports =  mongoose.model('Product', ProductSchema)
